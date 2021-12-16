745162ceb921d58a421ac53f153fa11a818a9a8b
Yanchick <yan@yanchick.org>
2021-12-16 22:09:58 +0500

    а вот и наш первый деплой

---
+++ src/old/tasks.py
@@ -0,0 +1,6 @@
+import time
+import random
+
+def foo(data):
+    time.sleep(40)
+    return random.randint(1,1000)
---
+++ src/web_server.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask import request
+import json
+
+
+app = Flask(__name__)
+
+
+@app.route("/",methods=['POST'])
+def hello_world():
+    return "<p>Привет участникам электива"
+
+@app.route("/parse/<job>")
+def parse_para(job):
+    return "Вы обратились на {}".format(job)
+
+@app.route("/sample_post",methods=["POST"])
+def parse_para_post():
+    d = json.loads(request.data)
+    return "Вы обратились по посту на {}".format(d['body'])
+
+
+app.run()
